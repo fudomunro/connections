2bd0a3e10b79f3e7253ab1bddde54c8c092e3889
fudomunro <alecmunro@gmail.com>
2021-04-20 09:55:15 -0300

    Task 2: add GET /connections and tests

--- connections/views.py
+++ connections/views.py
@@ -1,8 +1,9 @@
 from http import HTTPStatus
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from webargs.flaskparser import use_args
 
+from connections.models.connection import Connection
 from connections.models.person import Person
 from connections.schemas import ConnectionSchema, PersonSchema
 
@@ -21,6 +22,24 @@ def get_people():
 def create_person(person):
     person.save()
     return PersonSchema().jsonify(person), HTTPStatus.CREATED
+
+
+@blueprint.route('/connections', methods=['GET'])
+def get_connections():
+    # TODO: is many needed here?
+    connection_schema = ConnectionSchema(many=True)
+    people_schema = PersonSchema(many=True)
+    connections = Connection.query.all()
+    person_json = lambda p: dict([(f, getattr(p, f)) for f in ['id', 'first_name', 'last_name', 'email']])
+
+    # It didn't seem to make sense to alter the schema just to get an expanded representation of a person
+    return jsonify(list({
+        'id': connection.id,
+        'from_person': person_json(Person.query.filter(Person.id==connection.from_person_id).one()),
+        'to_person': person_json(Person.query.filter(Person.id==connection.to_person_id).one()),
+        'connection_type': connection.connection_type.value
+    }
+    for connection in connections)), HTTPStatus.OK
 
 
 @blueprint.route('/connections', methods=['POST'])
